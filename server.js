77dbf9d49e7781fe597b04c7e1fcabd1637d8f74
charbel abi khalil <charbelak31@gmail.com>
2024-05-01 00:31:40 +0300

    Initial commit of IDP project

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT;
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Gateway Service running on port ${PORT}`);
+});
