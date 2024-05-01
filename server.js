0ecb517d76be3ffee75834044808da1b11cafe11
charbel abi khalil <charbelak31@gmail.com>
2024-05-01 15:58:23 +0300

    1

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
-
+const auditTrail = require("./middleware/auditTrail");
 dotenv.config();
 const PORT = process.env.PORT;
 app.use(express.json());
+
+app.use(auditTrail);
 
 app.get("/", (req, res) => {
   res.send("Gateway is running");
